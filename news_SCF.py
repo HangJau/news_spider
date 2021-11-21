8b681ad1b4579e6adb512bcfd349323be73812e3
HangJau <48112437+HangJau@users.noreply.github.com>
2021-11-21 22:20:49 +0800

    对title 以及邮件进行排版

--- news_SCF.py
+++ news_SCF.py
@@ -1,63 +1,70 @@
-import datetime
+import re
 import requests
 from lxml import etree
+import time
 import yagmail
-from lxml.html import tostring
-import re
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4651.0 Safari/537.36'
+}
+
+# 获取日期
+timeStruct = time.localtime()
+strTime = time.strftime("%Y%m%d", timeStruct)
+str_time = int(strTime) - 1  # 如果是上午8点以后推送的，括号外的“-1”要删除
 
 
-
-def get_url(url):
-    # 抓取url
-    header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36",
-        "Host": "mrxwlb.com",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-    }
-    date_time = (datetime.date.today() + datetime.timedelta(0)).strftime("%Y-%#m-%#d").replace('-', '{}').format('年',
-                                                                                                                  '月') + "日新闻联播文字版"
-
-    url = url + "/{}".format(date_time)
+# 获取新闻
+def get_hanlder(url):
     try:
-        rsp = requests.get(url, headers=header, timeout=5)
+        rsp = requests.get(url, headers=headers, timeout=5)
         rsp.raise_for_status()
         rsp.encoding = rsp.apparent_encoding
-        # print(rsp.text)
         return rsp.text
     except requests.RequestException as error:
         print(error)
+        exit()
 
 
-def data_handle(data):
-    # 提取标签中的html格式
-    etr = etree.HTML(data)
-    news_content = etr.xpath('//div[@class="posts-wrapper"]/article')[0]
+def get_news(rsp):
+    etr = etree.HTML(rsp)
+    titles = etr.xpath("//div[@class='title']/text()")
+    hrefs = etr.xpath("//li/a/@href")
 
-    news_contens = tostring(news_content, encoding='utf-8').decode('utf-8')
-    # print(news_contens)
-    # print("*"*10)
-    news_text = re.sub("————–<br>[\\s\\S]*", "", news_contens)
+    news = []
+    summary = None
+    for title, href in zip(titles, hrefs):
+        if '《新闻联播》' in title:
+            # 获取新闻概要
+            title_rsp = get_hanlder(href)
+            summary = '<div>' + re.search(r'</em>本期节目主要内容：[\s\S]*。', title_rsp).group(
+                0) + "</div></br></br>"
+            continue
 
-    # 增加对敏感字符的处理
-    # news_text = re.sub('习近平', "习大大", news_text)
-    #
-    return news_text
+        # 新闻标题去掉视频并处理成超链接  橙色 #D2691E
+        tit = title.replace("[视频]", "")
+        subtitle = f'<a href=\"{href}\" style="color:#4682B4"><b>{tit}</b></a>'
+        summary = re.sub(tit, subtitle, summary)
+
+        # 获取新闻正文并添加到news
+        news_text = get_hanlder(href)
+        news_th = re.findall(r'.*(<div class="cnt_bd"><!--repaste.body.begin-->.*?</div>).*', news_text)[0]
+        news.append(f"{subtitle}\n{news_th}")
+
+    news[0] = summary
+    return news
 
 
 def email_send(rsp):
     # 发送数据到邮箱
-    yag = yagmail.SMTP(user='youremail', password='password', host='smtp.qq.com', port=465)
-
-    subject = (datetime.date.today() + datetime.timedelta(days=-1)).strftime("%Y-%m-%d") + " 新闻联播文字报"
-    yag.send(to=["xxxx@qq.com","xxx@qq.com"], subject=subject, contents=rsp)
+    yag = yagmail.SMTP(user='sendQQemail', password='emailpassword', host='smtp.qq.com', port=465)
+    yag.send(to=["receiveemai"], subject=f"{str_time}日新闻联播推送", contents=rsp)
     print('邮件已发送请查收...')
 
 
-def main(event, context):
-    # 程序执行入口
-    uri = "http://mrxwlb.com"
+def scf_run(event, context):
+    url = f'https://tv.cctv.com/lm/xwlb/day/{str_time}.shtml'
+    rsp = get_hanlder(url)
+    news_text = get_news(rsp)
+    email_send(news_text)
 
-    rsp = get_url(uri)
-    news_content = data_handle(rsp)
-    email_send(news_content)
-    return "邮件发送成功"
